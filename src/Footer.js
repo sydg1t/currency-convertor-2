ddceb4ff3f9809d04c3320b49b9135e057901d06
Obsydion <slaterobsydion@gmail.com>
2024-02-18 14:08:37 -0700

    Changed links in footer to target=_blank

--- src/Footer.js
+++ src/Footer.js
@@ -6,10 +6,10 @@ class Footer extends React.Component {
       
         
           <div id='footer' className='col-12 justify-content-center py-3'>
-            <a href='https://chipper-lily-87dd91.netlify.app/'>Portfolio</a>
+            <a href='https://chipper-lily-87dd91.netlify.app/' target='_blank'>Portfolio</a>
             <span>
-            <a href='https://github.com/sydg1t'>Github</a>
-            <a href='https://www.linkedin.com/in/obsydion-slater-07615525b/'>LinkedIn</a>
+            <a href='https://github.com/sydg1t' target='_blank'>Github</a>
+            <a href='https://www.linkedin.com/in/obsydion-slater-07615525b/' target='_blank'>LinkedIn</a>
             </span>
           </div>
         
